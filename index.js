89274bd85a63a54f9bdec33693017c36a3bc8aef
Vanessa Toro <vanetoro84@gmail.com>
2018-08-10 19:05:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const app = "I don't do much.";
+
+const token = 'da69607e4e80950fac9306049bffad8393ae01e8 '
+fetch('https://api.github.com/user/repos').
+  then(res => res.json()).
+  then(json => console.log(json))
